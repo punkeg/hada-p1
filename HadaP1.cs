b4eb379228cd4e7c5650fdadf033ee122db64331
Ege <yilmazege00@gmail.com>
2026-02-04 16:06:45 +0100

    Handle zero in Seconds2Minutes

--- HadaP1.cs
+++ HadaP1.cs
@@ -2,5 +2,15 @@
 
 public class HadaP1
 {
+    public static double Seconds2Minutes(double s)
+    {
+       if(s==0) return 0;
+        return 0;
+        
+        return s / 60.0;
+    }
 
 }
+
+
+
